e44cf1922c04056196b194a54bcaa5790af3699a
Daniil Petrov <waddlespinespig@gmail.com>
2022-06-26 18:33:56 +0000

    Improved log system

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,16 +1,15 @@
 using Microsoft.AspNetCore.SignalR;
 using Crykit.Classes;
-using System.IO;
 
 namespace Crykit.Hubs
 {
     public class ChatHub : Hub
     {
         public ChatHub(){
+            Directory.CreateDirectory("./logs");
         }
 
         private static Dictionary<string, List<Message>> msgLog = new Dictionary<string, List<Message>>();
-        private static StreamWriter sw = new StreamWriter("log.txt");
 
         public override async Task OnConnectedAsync(){
             Console.WriteLine("Client Added: " + Clients.Caller);
@@ -21,16 +20,29 @@ namespace Crykit.Hubs
         }
         public async Task SendMessage(string user, string message, string channel)
         {
-            user = user.Replace("\n", "\\n");
-            message = message.Replace("\n", "\\n");
-            channel = channel.Replace("\n", "\\n");
-            if(!ChatHub.msgLog.ContainsKey(channel)){
-                ChatHub.msgLog.Add(channel, new List<Message>());
+            if(channel != ""){
+                try{
+                    user = user.Replace("\n", "\\n");
+                    message = message.Replace("\n", "\\n");
+                    channel = channel.Replace("\n", "\\n");
+                    if(!ChatHub.msgLog.ContainsKey(channel)){
+                        ChatHub.msgLog.Add(channel, new List<Message>());
+                    }
+                    ChatHub.msgLog[channel].Add(new Message(user, message, channel));
+                    await Clients.All.SendAsync("ReceiveMessage", user, message, channel);
+                    string targetlog = "./logs/" + channel + ".txt";
+                    if(!File.Exists(targetlog)){
+                        await File.WriteAllTextAsync(targetlog, DateTime.Now.ToString() + "\n" + user + "\n" + message + "\n");
+                    }
+                    else{
+                        await File.AppendAllTextAsync(targetlog, DateTime.Now.ToString() + "\n" + user + "\n" + message + "\n");
+                    }
+                }
+                catch(Exception e){
+                    Console.WriteLine(e);
+                }
             }
-            ChatHub.msgLog[channel].Add(new Message(user, message, channel));
-            await Clients.All.SendAsync("ReceiveMessage", user, message, channel);
-            await sw.WriteLineAsync(DateTime.Now.ToString() + "\n" + channel + "\n" + user + "\n" + message);
-            await sw.FlushAsync();
+
         }
 
         public async Task GetChannelMessages(string channel){
